a9e540d178e78583b1dade9b7ab36e1241a5a1a0
Luca Pasini <luca.pasini@student.supsi.ch>
2020-03-04 11:08:53 +0100

    Text color adaptation based on bg. Bug still present

--- src/main/java/ch/supsi/labingsw1/model/StyledSelectableCell.java
+++ src/main/java/ch/supsi/labingsw1/model/StyledSelectableCell.java
@@ -8,25 +8,40 @@ import javafx.scene.layout.CornerRadii;
 
 import javafx.scene.paint.*;
 
-public class StyledCell extends ListCell<Course> {
-    private Color bgColor;
+public class StyledSelectableCell<T> extends ListCell<T> {
+    private Background selBG;
+    private Background idleBG;
+    private Paint idleTextCol;
+    private Paint selTextCol;
 
-    public StyledCell(Color bgColor) {
-        super();
-        this.bgColor = bgColor;
+    public StyledSelectableCell(Colors idleBG) {
+        assert idleBG.getComplementary() != null;
+        this.selBG = new Background(new BackgroundFill(idleBG.getComplementary().getVal(), CornerRadii.EMPTY, Insets.EMPTY));
+        this.idleBG = new Background(new BackgroundFill(idleBG.getVal(), CornerRadii.EMPTY, Insets.EMPTY));
+
+        this.idleTextCol = idleBG.getSuggestedTextColor();
+        this.selTextCol = idleBG.getComplementary().getSuggestedTextColor();
     }
 
     @Override
-    protected void updateItem(Course item, boolean empty) {
+    protected void updateItem(T item, boolean empty) {
         super.updateItem(item, empty);
-        this.setBackground(
-                new Background(new BackgroundFill(bgColor, CornerRadii.EMPTY, Insets.EMPTY))
-        );
 
-        this.setTextFill(Colors.WHITE.getVal());
+        if(item == null) return;
+
+        if(this.isSelected()) {
+            setBG(this.selBG);
+            setTextFill(this.selTextCol);
+        }
+        else {
+            setBG(this.idleBG);
+            setTextFill(this.idleTextCol);
+        }
+
+        setText(item.toString());
     }
 
-    public void setColor(Colors c){
-        this.bgColor = c.getVal();
+    private void setBG(Background c){
+        this.setBackground(c);
     }
 }
