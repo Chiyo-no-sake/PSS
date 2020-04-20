9916279b94adfe9707ffa6ae26544bb1913e13d5
Luca Pasini <luca.pasini@student.supsi.ch>
2020-04-20 23:22:38 +0200

    directory reorganized and restructured

---
+++ pss/frontend/src/main/java/ch/supsi/pss/Pss.java
@@ -0,0 +1,7 @@
+package ch.supsi.pss;
+
+public class Pss {
+    public static void main(String[] args) {
+        PssFX.launch(args);
+    }
+}
---
+++ pss/frontend/src/main/java/ch/supsi/pss/PssFX.java
@@ -0,0 +1,12 @@
+package ch.supsi.pss;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class PssFX extends Application {
+
+    @java.lang.Override
+    public void start(Stage stage) {
+
+    }
+}
