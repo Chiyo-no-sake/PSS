a013a8d1e9e77c0d50bc725e502d7a6e8f623f7e
Manuel <manuel.grgicfigueiredo@student.supsi.ch>
2020-04-26 16:14:28 +0200

    first sketch

--- pss/frontend/src/main/java/ch/supsi/pss/PssFX.java
+++ pss/frontend/src/main/java/ch/supsi/pss/PssFX.java
@@ -3,13 +3,15 @@ package ch.supsi.pss;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import javafx.scene.layout.GridPane;
 import javafx.scene.canvas.*;
 
 public class PssFX extends Application {
@@ -21,28 +23,35 @@ public class PssFX extends Application {
     @java.lang.Override
     public void start(Stage stage) {
         stage.setTitle("Draw.io");
-        StackPane drawPane = new StackPane();
-        StackPane galleryPane = new StackPane();
+        GridPane drawPane = new GridPane();
+        GridPane galleryPane = new GridPane();
 
+        Scene drawScene = new Scene(drawPane, 800, 600, Color.BLACK);
+        Scene galleryScene = new Scene(galleryPane, 800, 600, Color.BLUE);
+
+        //Labels
+        Label drawTitle = new Label("Draw Window");
+        Label galleryTitle = new Label("Gallery Window");
+
+        //Buttons
         Button drawBtn = new Button();
-        drawBtn.setText("Draw");
+        drawBtn.setText("Go to Draw Window");
         drawBtn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
-                System.out.println("Draw Scene");
+                stage.setScene(drawScene);
             }
         });
-        drawBtn.setVisible(true);
 
         Button galleryBtn = new Button();
-        galleryBtn.setText("Gallery");
+        galleryBtn.setText("Go to Gallery Window");
         galleryBtn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
-                System.out.println("Gallery Scene");
+                stage.setScene(galleryScene);
+                stage.sizeToScene();
             }
         });
-        galleryBtn.setVisible(true);
 
         Button saveBtn = new Button();
         saveBtn.setText("Save");
@@ -51,25 +60,49 @@ public class PssFX extends Application {
             @Override
             public void handle(ActionEvent actionEvent) {
                 System.out.println("Drawing saved");
+                stage.sizeToScene();
             }
 
         });
 
-        Canvas canvas = new Canvas(250,250);
+        //Canvas
+        Pane canvasPane = new Pane();
+        Canvas canvas = new Canvas();
+        canvas.setHeight(500);
+        canvas.setWidth(canvasPane.getWidth());
         GraphicsContext gc = canvas.getGraphicsContext2D();
+        canvasPane.setStyle("-fx-border-style: solid;" +
+                "-fx-border-color: black;" +
+                "-fx-background-color: white;");
+        canvasPane.getChildren().add(canvas);
 
-        HBox sceneChanger = new HBox();
-        sceneChanger.getChildren().addAll(drawBtn, galleryBtn);
+        //Search Field
+        TextField search = new TextField();
 
-        drawPane.getChildren().addAll(sceneChanger, canvas, saveBtn);
+        //Draw Window Layout
+        VBox vbox = new VBox();
+        vbox.prefWidthProperty().bind(stage.widthProperty());
+        vbox.prefHeightProperty().bind(stage.heightProperty());
+        vbox.setSpacing(10);
+        vbox.setPadding(new Insets(0, 20, 10, 20));
+        vbox.getChildren().addAll(drawTitle, galleryBtn, canvasPane, saveBtn);
+        vbox.setAlignment(Pos.CENTER);
 
-        galleryPane.getChildren().add(drawBtn);
-        galleryPane.getChildren().add(galleryBtn);
+        drawPane.getChildren().add(vbox);
 
-        Scene drawScene = new Scene(drawPane);
-        Scene galleryScene = new Scene(galleryPane, 300, 300, Color.BLUE);
+        //Gallery Window Layout
+        VBox vbox2 = new VBox();
+        vbox2.prefWidthProperty().bind(stage.widthProperty());
+        vbox2.prefHeightProperty().bind(stage.heightProperty());
+        vbox2.setSpacing(10);
+        vbox2.setPadding(new Insets(0, 20, 10, 20));
+        HBox images = new HBox();
+        vbox2.getChildren().addAll(galleryTitle, drawBtn, search, images);
+        vbox2.setAlignment(Pos.CENTER);
+        galleryPane.getChildren().add(vbox2);
 
         stage.setScene(drawScene);
+        stage.sizeToScene();
         stage.show();
     }
 
